057ea66b8c5cb7767fd2774b3b74f004802c0fa5
Jeffrey T Gosselin <jgoz.email@gmail.com>
2018-11-20 00:49:13 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,11 @@
 def oxford_comma(array)
   if array.length == 1 
     array.join()
+  end
   
-  elsif array.length == 2 
+  if array.length == 2 
     array.join(" and ")
+  end
   
   elsif array.length == 3 
     
