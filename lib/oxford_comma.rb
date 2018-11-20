7592951d5c2a1bb802316b870ebe90155493f45a
Jeffrey T Gosselin <jgoz.email@gmail.com>
2018-11-20 01:00:49 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,16 +2,14 @@ def oxford_comma(array)
   num = array.size
   
   case num
-    when num == 1
+  when num == 1
     array.join()
   
-  elsif array.length == 2 
+  when num == 2 
     array.join(" and ")
   
-  elsif array.length == 3 
+  when num == 3 
+    puts num
     
     
-    
-    
-  
 end
